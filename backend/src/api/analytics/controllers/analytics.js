f0036b79d2cff85108bb5e478f0e5bc0c1353ea9
Naman Dureja <durejanaman29@gmail.com>
2022-11-23 14:02:02 +0530

    anal update

--- backend/src/api/analytics/controllers/analytics.js
+++ backend/src/api/analytics/controllers/analytics.js
@@ -7,20 +7,25 @@ module.exports = createCoreController("api::analytics.analytics", ({ strapi }) =
     send(ctx) {
         const payload = ctx.request.body;
 
-        if (!payload.client_id) {
-            return ctx.badRequest('client_id is required');
-        }
-        if (!payload.events || !Array.isArray(payload.events)) {
-            return ctx.badRequest('events are required');
-        }
+        fetch(`https://www.google-analytics.com/mp/collect?measurement_id=G-SC82Z7RD6Y&api_secret=ucjKJE3CTYmHEk1WGYsjdQ`, {
+            method: "POST",
+            body: JSON.stringify(payload)
+        });
 
-        strapi
-            .plugin('measurement-protocol')
-            .service('gtag')
-            .send({
-                user_id: ctx?.state?.user?.id,
-                ...payload,
-            });
+        // if (!payload.client_id) {
+        //     return ctx.badRequest('client_id is required');
+        // }
+        // if (!payload.events || !Array.isArray(payload.events)) {
+        //     return ctx.badRequest('events are required');
+        // }
+
+        // strapi
+        //     .plugin('measurement-protocol')
+        //     .service('gtag')
+        //     .send({
+        //         user_id: ctx?.state?.user?.id,
+        //         ...payload,
+        //     });
 
         return 'OK';
     },
